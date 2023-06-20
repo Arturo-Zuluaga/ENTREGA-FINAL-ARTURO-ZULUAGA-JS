00844c23601ebbdf50f408eed837d77f240afb53
Arturo Zuluaga <mallecum@gmail.com>
2023-06-20 11:58:40 -0300

    otro commit

---
+++ js/productos.js
@@ -0,0 +1,107 @@
+const productos = [
+	{
+	  id: 1,
+	  nombre: "caña de pescar",
+	  presentacion: "unidades",
+	  precio: 1000,
+	  img: "../img/1.jpg",
+	},
+	{
+	  id: 2,
+	  nombre: "plomos",
+	  presentacion: "docenas",
+	  precio: 200,
+	  img: "../img/2.jpg"
+	},
+	{
+	  id: 3,
+	  nombre: "anzuelos",
+	  presentacion: "promociones",
+	  precio: 100,
+	  img: "../img/3.jpg"
+	},
+	{
+	  id: 4,
+	  nombre: "carretes",
+	  presentacion: "unidades ",
+	  precio: 2000,
+	  img: "../img/4.jpg"
+	},
+	{
+	  id: 5,
+	  nombre: "flotantes",
+	  presentacion: "docenas ",
+	  precio: 200,
+	  img: "../img/5.webp"
+	},
+	{
+	  id: 6,
+	  nombre: "caja porta equipo",
+	  presentacion: "promociones",
+	  precio: 800,
+	  img: "../img/6.jpg"
+	},
+	{
+	  id: 7,
+	  nombre: "pinzas",
+	  presentacion: "unidades ",
+	  precio: 300,
+	  img: "../img/7.jpg"
+	},
+	{
+	  id: 8,
+	  nombre: "saca vueltas",
+	  presentacion: "docenas",
+	  precio: 120,
+	  img: "../img/8.webp"
+	},
+	{
+	  id: 9,
+	  nombre: "señuelos varios",
+	  presentacion: "promociones",
+	  precio: 600,
+	  img: "../img/9.webp"
+	},
+	{
+	  id: 10,
+	  nombre: "nylon",
+	  presentacion: "unidades",
+	  precio: 110,
+	  img: "../img/10.webp"
+	},
+	{
+	  id: 11,
+	  nombre: "carnada",
+	  presentacion: "docenas",
+	  precio: 200,
+	  img: "../img/11.jpg"
+	},
+	{
+	  id: 12,
+	  nombre: "alicate",
+	  presentacion: "promociones",
+	  precio: 300,
+	  img: "../img/12.jpg"
+	},
+	{
+	  id: 13,
+	  nombre: "Cuchillos",
+	  presentacion: "unidades",
+	  precio: 450,
+	  img: "../img/13.jpeg"
+	},
+	{
+	  id: 14,
+	  nombre: "gorro",
+	  presentacion: "unidades",
+	  precio: 500,
+	  img: "../img/14.jpeg"
+	},
+	{
+	  id: 15,
+	  nombre: "Cantimplora",
+	  presentacion: "unidades",
+	  precio: 500,
+	  img: "../img/15.jpeg"
+	},
+  ];
